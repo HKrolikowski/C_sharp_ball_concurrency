e7776929c424509233e3a935d1fafd3261906396
HKrolikowski <hubertczacha4@gmail.com>
2022-03-17 20:10:32 +0100

    Dodanie klasy Bajt i uzupelnienie funkcji main

---
+++ projekt_wsp/Bajt.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt_wsp
+{
+    internal class Bajt
+    {
+        private Byte _wartosc;
+        public int Wartosc
+        {
+            get => _wartosc;
+            set
+            {
+                if ((value >= 0) && (value < 256))
+                {
+                    _wartosc = (Byte) value;
+                }
+            }
+        }
+
+        public Bajt(Byte wartosc)
+        {
+            _wartosc = wartosc;
+        }
+
+        //Sprawdzenie co znajduje sie na danym bicie
+        public bool isBit(int pozycja) //pozycja od prawej
+        {
+            if (pozycja >= 0 && pozycja < 8)
+            {
+                return (_wartosc & (1 << pozycja)) != 0;
+            }
+            else return false;
+        }
+
+        //wstawienie 0 na dana pozycje w bajcie
+        public void setBitZero(int pozycja)
+        {
+            if(pozycja >= 0 && pozycja < 8)
+            {
+                _wartosc = (Byte) (_wartosc & ~(1 << pozycja));
+            }
+        }
+
+
+        //wstawianie 1 na dana pozycje w bajcie
+        public void setBitOne(int pozycja)
+        {
+            if (pozycja >= 0 && pozycja < 8)
+            {
+                _wartosc = (Byte) (_wartosc | 1 << pozycja);
+            }  
+        }
+    }
+}
--- projekt_wsp/Program.cs
+++ projekt_wsp/Program.cs
@@ -1,10 +1,27 @@
-﻿
-//Console.WriteLine(add(4, 6));
+﻿using System;
 
-
-int add(int a, int b)
+namespace projekt_wsp
 {
-    return a + b;
-}
 
+    class Program
+    {
+        //Console.WriteLine();
+        static void Main(string[] args)
+        {
+            Bajt bajt = new Bajt(65);
+            Console.WriteLine("Bajt ma wartosc: " + bajt.Wartosc);
+            bajt.setBitZero(0);
+            Console.WriteLine("Bajt ma wartosc: " + bajt.Wartosc);
+            bajt.setBitOne(0);
+            Console.WriteLine("Bajt ma wartosc: " + bajt.Wartosc);
+            if (bajt.isBit(0) == true)
+            {
+                Console.WriteLine("Bajt na pozycji 0 ma 1");
+            }
+            else Console.WriteLine("Bajt na pozycji 0 ma 0");
+
+
+        }
+    }
+}
 
